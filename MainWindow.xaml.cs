b09360f4a33bb5e3d156732ec37ac83e566bbd04
gorba <gorba@NITRO-ENCELAD>
2022-03-20 22:41:59 +0200

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net;
+using System.Net.Sockets;
+
+namespace RegistrationClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!this.Login_TextBox.Text.Contains("@gmail.com"))
+            {
+                MessageBox.Show("Incorrect data! Login must contains @gmail.com !");
+            }
+            else if (this.PasswordBox.Password.Length < 6)
+            {
+                MessageBox.Show("Incorrect data! Password must be not shorter than 6 !");
+            }
+            else
+            {
+                string data = JsonSerializer.Serialize(new User(this.Login_TextBox.Text,this.HashString(this.PasswordBox.Password)));
+                MessageBox.Show(data);
+                SendData(data);
+            }
+        }
+
+        private string HashString(string text, string salt = "")
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return String.Empty;
+            }
+
+            // Uses SHA256 to create the hash
+            using (var sha = new System.Security.Cryptography.SHA256Managed())
+            {
+                // Convert the string to a byte array first, to be processed
+                byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(text + salt);
+                byte[] hashBytes = sha.ComputeHash(textBytes);
+
+                // Convert back to a string, removing the '-' that BitConverter adds
+                string hash = BitConverter
+                    .ToString(hashBytes)
+                    .Replace("-", String.Empty);
+
+                return hash;
+            }
+        }
+
+        private void SendData(string data)
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 6967);
+
+            // Create a TCP/IP  socket.
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            sender.Connect(remoteEP);
+            sender.Send(Encoding.UTF8.GetBytes(data));
+        }
+    }
+}
